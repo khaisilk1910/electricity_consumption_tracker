23a70b95b321d1b78d37ee21a2d0dcab34bf2a48
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-01-18 21:36:07 +0700

    Add configuration flow for electricity consumption tracker

---
+++ custom_components/electricity_consumption_tracker/config_flow.py
@@ -0,0 +1,23 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.helpers import selector
+from .const import DOMAIN, CONF_SOURCE_SENSOR, CONF_UPDATE_INTERVAL, CONF_FRIENDLY_NAME
+
+class ConsumptionTrackerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_FRIENDLY_NAME], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_FRIENDLY_NAME): str,
+                vol.Required(CONF_SOURCE_SENSOR): selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain="sensor", device_class="energy")
+                ),
+                vol.Required(CONF_UPDATE_INTERVAL, default=1): selector.NumberSelector(
+                    selector.NumberSelectorConfig(min=1, max=24, step=1, unit_of_measurement="giờ")
+                ),
+            })
+        )
