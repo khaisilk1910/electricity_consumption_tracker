d203b3e76d00aaa090b12f50996582fb18060613
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-01-18 21:35:40 +0700

    Create const.py

---
+++ custom_components/electricity_consumption_tracker/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "electricity_consumption_tracker"
+CONF_SOURCE_SENSOR = "source_sensor"
+CONF_UPDATE_INTERVAL = "update_interval"
+CONF_FRIENDLY_NAME = "friendly_name"
+
+PRICE_HISTORY = {
+    "2019-03-20": [(50, 1678), (50, 1734), (100, 2014), (100, 2536), (100, 2834), (float('inf'), 2927)],
+    "2023-05-04": [(50, 1806), (50, 1866), (100, 2167), (100, 2729), (100, 3050), (float('inf'), 3151)],
+    "2024-10-11": [(50, 1893), (50, 1956), (100, 2271), (100, 2860), (100, 3197), (float('inf'), 3302)],
+    "2025-05-10": [(50, 1984), (50, 2050), (100, 2380), (100, 2998), (100, 3350), (float('inf'), 3460)]
+}
