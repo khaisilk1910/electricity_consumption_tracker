2a4b05c3ab95bc88f5480416f3efe02403b6422f
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-01-18 23:39:36 +0700

    Update const.py

--- custom_components/electricity_consumption_tracker/const.py
+++ custom_components/electricity_consumption_tracker/const.py
@@ -3,6 +3,7 @@ CONF_SOURCE_SENSOR = "source_sensor"
 CONF_UPDATE_INTERVAL = "update_interval"
 CONF_FRIENDLY_NAME = "friendly_name"
 
+# Biểu giá điện lịch sử theo file mẫu của bạn
 PRICE_HISTORY = {
     "2019-03-20": [(50, 1678), (50, 1734), (100, 2014), (100, 2536), (100, 2834), (float('inf'), 2927)],
     "2023-05-04": [(50, 1806), (50, 1866), (100, 2167), (100, 2729), (100, 3050), (float('inf'), 3151)],
