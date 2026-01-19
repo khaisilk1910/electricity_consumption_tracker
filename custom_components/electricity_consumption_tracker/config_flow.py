b020cd947004a0cfe3477594f0f04fcc42e7b8ab
khaisilk1910 <3167468+khaisilk1910@users.noreply.github.com>
2026-01-19 08:11:26 +0700

    Update config_flow.py

--- custom_components/electricity_consumption_tracker/config_flow.py
+++ custom_components/electricity_consumption_tracker/config_flow.py
@@ -1,3 +1,4 @@
+"""Config flow for Electricity Consumption Tracker."""
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
@@ -8,20 +9,22 @@ class ConsumptionTrackerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
             return self.async_create_entry(title=user_input[CONF_FRIENDLY_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_FRIENDLY_NAME): str,
+                vol.Required(CONF_FRIENDLY_NAME, default="Electricity Home"): str,
                 vol.Required(CONF_SOURCE_SENSOR): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor", device_class="energy")
+                    selector.EntitySelectorConfig(domain="sensor") # Cho phép chọn nhiều loại sensor hơn
                 ),
                 vol.Required(CONF_UPDATE_INTERVAL, default=1): selector.NumberSelector(
                     selector.NumberSelectorConfig(min=1, max=24, step=1, unit_of_measurement="giờ")
                 ),
-            })
+            }),
+            errors=errors
         )
 
     @staticmethod
